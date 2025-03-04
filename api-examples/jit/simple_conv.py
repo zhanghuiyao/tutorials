385f81f2caaee3aa4f50eedd5f99514f5f8410c0
zhanghuiyao <1814619459@qq.com>
2025-03-04 16:17:20 +0800

    1

--- api-examples/jit/simple_conv.py
+++ api-examples/jit/simple_conv.py
@@ -69,16 +69,13 @@ def run_func(block: nn.Cell, des:str = "function"):
 
     time_to_run_ten_times = time.time() - s_time
 
-    print(f"{des}, \
-          output shape is: {out.shape}, \
-          time to compile: {time_to_compile:.2f}s, \
-          time to run thousand times: {time_to_run_ten_times:.2f}s, \
+    print(f"{des}, output shape is: {out.shape}, time to compile: {time_to_compile:.2f}s, time to run thousand times: {time_to_run_ten_times:.2f}s, 
           time end to end(a thousand times): {time_to_compile+time_to_run_ten_times:.2f}")
 
 
 block = BasicBlock()
 
-run_func(block, des="origin block")
+# run_func(block, des="origin block")
 
 block.construct = mindspore.jit(block.construct)
 
