432cb6e37abeccd70d8f8eb9dbb17aa5cfd1a507
zhanghuiyao <1814619459@qq.com>
2025-03-04 16:20:32 +0800

    1

--- api-examples/jit/simple_conv.py
+++ api-examples/jit/simple_conv.py
@@ -69,8 +69,8 @@ def run_func(block: nn.Cell, des:str = "function"):
 
     time_to_run_ten_times = time.time() - s_time
 
-    print(f"{des}, output shape is: {out.shape}, time to compile: {time_to_compile:.2f}s, time to run thousand times: {time_to_run_ten_times:.2f}s, 
-          time end to end(a thousand times): {time_to_compile+time_to_run_ten_times:.2f}")
+    print(f"{des}, output shape is: {out.shape}, time to compile: {time_to_compile:.2f}s, time to run thousand times: {time_to_run_ten_times:.2f}s, "
+          f"time end to end(a thousand times): {time_to_compile+time_to_run_ten_times:.2f}")
 
 
 block = BasicBlock()
