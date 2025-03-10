a05118869cb48a9b54759da73b0e46d5a6425dac
zhanghuiyao <1814619459@qq.com>
2025-03-10 19:13:15 +0800

    1

--- parallel-examples/code/pipeline-parallelism.py
+++ parallel-examples/code/pipeline-parallelism.py
@@ -11,7 +11,7 @@ from mindspore.nn.utils import no_init_parameters
 context.set_context(mode=context.GRAPH_MODE, pynative_synchronize=True)
 # mindspore.set_auto_parallel_context(parallel_mode=mindspore.ParallelMode.SEMI_AUTO_PARALLEL, pipeline_stages=4)
 # mindspore.set_auto_parallel_context(pipeline_config={'pipeline_scheduler':'1f1b', 'pipeline_interleave':True})
-# init()
+init()
 
 
 class Mlp(nn.Cell):
@@ -45,28 +45,27 @@ class Mlp(nn.Cell):
 
 
 net = Mlp(num_layers=8)
-optimizer = nn.SGD(net.trainable_params(), learning_rate=0.1)
+optimizer = nn.SGD(net.trainable_params(), learning_rate=0.01)
 
 
 # pipeline-parallelism setting
-# stage_config = {
-#     "layers.0": 0, "layers.1": 0,   # stage 0
-#     "layers.2": 1, "layers.3": 1,   # stage 1
-#     "layers.4": 2, "layers.5": 2,   # stage 2
-#     "layers.6": 3, "layers.7": 3, "loss_fn": 3  # stage 3
-# }
-# pp_net = nn.PipelineCell(net, micro_size=4, stage_config=stage_config)
-# pp_net = AutoParallel(pp_net, parallel_mode="semi_auto")
-# pp_net.full_batch = True
-# pp_net.pipeline(stages=4, scheduler="1f1b", interleave=True)
-# pp_net.set_train()
-
-pp_net = net
+stage_config = {
+    "layers.0": 0, "layers.1": 0,   # stage 0
+    "layers.2": 1, "layers.3": 1,   # stage 1
+    "layers.4": 2, "layers.5": 2,   # stage 2
+    "layers.6": 3, "layers.7": 3, "loss_fn": 3  # stage 3
+}
+pp_net = nn.PipelineCell(net, micro_size=4, stage_config=stage_config)
+pp_net = AutoParallel(pp_net, parallel_mode="semi_auto")
+pp_net.full_batch = True
+pp_net.pipeline(stages=4, scheduler="1f1b", interleave=True)
 pp_net.set_train()
 
+# pp_net = net
+# pp_net.set_train()
+
 grad_fn = ops.value_and_grad(pp_net, None, optimizer.parameters)
-# pp_grad_reducer = nn.PipelineGradReducer(optimizer.parameters)
-pp_grad_reducer = nn.Identity()
+pp_grad_reducer = nn.PipelineGradReducer(optimizer.parameters)
 
 
 @mindspore.jit
