d97a8f426b0cd18b976c74262d4df79bf2dccb44
zhanghuiyao <1814619459@qq.com>
2025-03-12 11:16:37 +0800

    1

--- api-examples/jit/simple_conv.py
+++ api-examples/jit/simple_conv.py
@@ -72,7 +72,8 @@ def fp_and_bp(x):
 def run_func(f: Callable, des:str = "function"):
     s_time = time.time()
 
-    dataset = [Tensor(np.ones((1, 128, 256, 256))*np.random.randn(1), mindspore.float32) for _ in range(1000)]
+    tmp = np.random.randn(1, 128, 256, 256)
+    dataset = [Tensor(tmp*np.random.randn(1), mindspore.float32) for _ in range(1000)]
     print(f"time init dataset: {time.time()-s_time:.2f}s")
 
     out = f(dataset[0])
